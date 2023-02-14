7832c2a89a50a4c281ad5baf86bb68222907b53a
iamDuckduck <125134849+iamDuckduck@users.noreply.github.com>
2023-02-14 18:06:44 +0800

    Add files via upload

--- promptDrawing.py
+++ promptDrawing.py
@@ -6,7 +6,7 @@ import os
 
 
 def promptForDrawing1(bot, gender, hair, eye, clothes, trait, name):
-    os.environ["REPLICATE_API_TOKEN"] = "API Token"
+    os.environ["REPLICATE_API_TOKEN"] = "4e43941c916c15479dd0d72421a8c51c5a905bba"
     image = "Please list no more than 6 keywords from our conversation, we will use them for image generator."
 
     while True:
@@ -127,3 +127,49 @@ def promptForDrawing2(bot, hair, eye, name):
         else:
             response = bot.ask(user_input)
             print(name, ": ", response)
+
+def previewDrawing(hair, eye):
+    model = replicate.models.get("cjwbw/anything-v3-better-vae")
+    version = model.versions.get("09a5805203f4c12da649ec1923bb7729517ca25fcac790e640eaa9ed66573b65")
+
+    # https://replicate.com/cjwbw/anything-v3-better-vae/versions/09a5805203f4c12da649ec1923bb7729517ca25fcac790e640eaa9ed66573b65#input
+    inputs = {
+        # Input prompt
+        'prompt': "masterpiece, best quality, 1girl, age friendly " + hair + eye,
+
+        # The prompt or prompts not to guide the image generation (what you do
+        # not want to see in the generation). Ignored when not using guidance.
+        # 'negative_prompt': ...,
+
+        # Width of output image. Maximum size is 1024x768 or 768x1024 because
+        # of memory limits
+        'width': 512,
+
+        # Height of output image. Maximum size is 1024x768 or 768x1024 because
+        # of memory limits
+        'height': 512,
+
+        # Number of images to output
+        'num_outputs': 1,
+
+        # Number of denoising steps
+        # Range: 1 to 500
+        'num_inference_steps': 20,
+
+        # Scale for classifier-free guidance
+        # Range: 1 to 20
+        'guidance_scale': 7,
+
+        # Choose a scheduler.
+        'scheduler': "DPMSolverMultistep",
+
+        # Random seed. Leave blank to randomize the seed
+        # 'seed': ...,
+    }
+
+    # https://replicate.com/cjwbw/anything-v3-better-vae/versions/09a5805203f4c12da649ec1923bb7729517ca25fcac790e640eaa9ed66573b65#output-schema
+    output = version.predict(**inputs)
+    response = requests.get(output[0])
+    img = Image.open(BytesIO(response.content))
+    img.show()
+    return 0
--- promptModule.py
+++ promptModule.py
@@ -1,5 +1,6 @@
 from promptDrawing import promptForDrawing1
 from promptDrawing import promptForDrawing2
+from promptDrawing import previewDrawing
 from chatgpt_wrapper import ChatGPT ##https://stackoverflow.com/questions/31235376/pycharm-doesnt-recognize-installed-module, if chatgpt_wrapper not working try to find solution from the link
 import os
 
@@ -25,14 +26,18 @@ def promptSetUp(userPrompt):##receive prompt setting from the
 
     if userPrompt == '2':
         bot = ChatGPT()
-        name = input("What is the name of your waifu? ")
-        series = input("from what series? ")
-        hair = (input("What is the hair color of your waifu? ") + " hair")
-        eye = (input("What is the eye color of your waifu? ") + " eyes")
+        preview = 1
+        while preview == 1:
+            name = input("What is the name of your waifu? ")
+            series = input("from what series? ")
+            hair = (input("What is the hair color of your waifu? ") + " hair")
+            eye = (input("What is the eye color of your waifu? ") + " eyes")
+            previewDrawing(hair, eye)
+            preview = input('press 1 if you want to regenerate your waifu, else press 2')
         print("Generating your waifu, please wait......")
         bot.ask(
             "I want you to act like " + name + " from " + series + ". I want you to respond and answer like " + name + ". Using the tone, manner and vocabulary " + name + " would use. Do not write any explanations. Only answer like " + name + ". You must know all of the knowledge of " + name + "." + "My first sentence is Hi " + name +".")
         temp = bot.ask(f'User: Hi {name}')
-        print(f"Rem: {temp}")
+        print(f"{name}: {temp}")
 
         promptForDrawing2(bot, hair, eye, name)
